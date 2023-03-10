40926c4cc1730bbbf81c06fbcb8b481f2de86f58
ximmanuel <immanuelmruk@gmail.com>
2023-03-10 15:40:33 +0100

    add footer news letter

--- src/Resources/app/storefront/src/scripts/footer/collapse-footer.js
+++ src/Resources/app/storefront/src/scripts/footer/collapse-footer.js
@@ -4,25 +4,28 @@ import DeviceDetection from 'src/helper/device-detection.helper';
 
 export default class CollapseFooter extends Plugin {
     init() {
-        this._uncollapseFooterIcons = DomAccess.querySelectorAll(document, '.uncollapse-footer-icon');
-        this._collapseFooterIcons = DomAccess.querySelectorAll(document, '.collapse-footer-icon');
+        this._uncollapseFooterIcon = DomAccess.querySelector(document, '.uncollapse-footer-icon');
+        this._collapseFooterIcon = DomAccess.querySelector(document, '.collapse-footer-icon');
 
         this.registerEvents()
     }
 
     registerEvents() {
         if (DeviceDetection.isTouchDevice()) {
-            for (this._uncollapseFooterIcon of this._uncollapseFooterIcons) {
+            this._uncollapseFooterIcon.addEventListener('click', this._uncollapseFooter.bind(this));
+            this._collapseFooterIcon.addEventListener('click', this._collapseFooter.bind(this));
+
+            /* for (this._uncollapseFooterIcon of this._uncollapseFooterIcons) {
                 this._uncollapseFooterIcon.addEventListener('click', this._uncollapseFooter.bind(this));
             }
             for (this._collapseFooterIcon of this._collapseFooterIcons) {
                 this._collapseFooterIcon.addEventListener('click', this._collapseFooter.bind(this));
-            }
+            } */
         }
     }
 
     _uncollapseFooter() {
-        this._footerBlockContent = this._uncollapseFooterIcon.getAttr('id')
+        this._footerBlockContent = DomAccess.querySelector(document, '.footer-block-content');
 
         this._footerBlockContent.classList.remove('d-none');
 
